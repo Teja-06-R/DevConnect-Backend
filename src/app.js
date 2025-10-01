639810ea5ccfeaf8177093fb6e4447b361fb4d28
Himateja <reddihimateja06@gmail.com>
2025-10-01 19:19:55 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,43 @@
 const express=require('express');
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send("Namaste user");
-})
+
 app.get('/hello',(req,res)=>{
     res.send("Hello user");
 })
-app.get('/test/ok',(req,res)=>{
+app.get('/users/:userId/books/:bookId', (req, res) => {
+  res.send(req.params)
+})
+
+app.get(/.*fly$/, (req, res) => {
+  res.send('/.*fly$/')
+})
+
+app.use('/test/ok',(req,res)=>{
     res.json({ message: "Namaste Testing...", status: "Ok" });
+})
+app.post('/hello',(req,res)=>{
+    res.send({'firstname':'Himateja',
+        'Lastname':'Reddi',
+    });
+})
+app.put('/hello',(req,res)=>{
+    res.send({'firstname':'Himateja',
+        'Lastname':'Reddi',
+        'age':21
+    });
+})
+app.patch('/hello',(req,res)=>{
+    res.send({'firstname':'Himateja',
+        'Lastname':'Reddi',
+        'Interest':'solving real world Problems using MERN'
+    });
+})
+app.delete('/hello',(req,res)=>{
+    res.send("Successfully Deleted!");
+})
+app.use('/',(req,res)=>{
+    res.send("Namaste user");
 })
 
 app.listen(7777,()=>{
